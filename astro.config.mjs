cb052f8041777685e908b9ab963b87c3c155dc5d
curtis-murray <murrcurt09@gmail.com>
2024-01-24 01:10:54 +1030

    reading docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://curtis-murray.github.io',
+  site: 'https://curtis-murray.github.io',
   integrations: [tailwind(), react()],
   vite: {
     ssr: {
       noExternal: ["react-icons"],
     },
   },
-  outDir: './docs',
 });
